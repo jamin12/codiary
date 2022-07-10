01b9e4ce41b2be5a279b37d128bf5e18fd2571ab
Hyohyeon Lim <limhyeon0809@gmail.com>
2022-07-11 02:32:18 +0900

    FEAT: OwlCarousel 기능 추가
    아직 기능 구현 중,
    백엔드에서 받아오는 정보를 뿌리는 기능은 조금 미루고 디자인 적인 부분만 조금 더 수정해야함.

--- client/src/Pages/Home/Popular.js
+++ client/src/Pages/Home/Popular.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import PopOwlcarousel from '../../components/PopOwlcarousel'
 
 const HomeTitle = styled.h1`
     font-size: 2.5rem;
@@ -9,22 +10,13 @@ const HomeTitle = styled.h1`
     color: black;
     padding-top: 5%
 `
-const PopularityText = styled.div `
-    width: 80%;
-    height: 60vh;
-    margin-top:40px;
-    margin-left: auto;
-    margin-right: auto;
-    background-color: pink;
-
-`
 
 
 const Popular = () => {
   return (
     <div className='container' id='popularity-text'>
         <HomeTitle>인기 게시글</HomeTitle>
-        <PopularityText>인기 게시글 슬라이드 바 들어가야 함</PopularityText>
+        <PopOwlcarousel/>
     </div>
   )
 }
