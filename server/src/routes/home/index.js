2a24a81bff26c3bea1dcd57f7398301b1d287c5f
jamin12 <rudals9901@naver.com>
2022-07-14 02:41:23 +0900

    style : 라우터 변경

--- server/src/routes/home/index.js
+++ server/src/routes/home/index.js
@@ -7,7 +7,6 @@ const { isLoggedIn,isNotLoggedIn } = require('../middleware');
 
 router.get("/",isLoggedIn,ctrl.output.index);
 router.get("/login",ctrl.output.login);
-router.get("/oauth2",ctrl.output.oauth2);
 router.get("/oauth2callback",ctrl.output.oauth2callback);
 router.get("/logout",isLoggedIn,ctrl.output.logout)
 
