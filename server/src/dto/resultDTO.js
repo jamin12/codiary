bd88319df0d517e6976ed0cb53bd9cdeaa5d8f7a
jamin12 <rudals9901@naver.com>
2022-07-14 02:39:35 +0900

    style : 필요없는 함수 삭제
    resultDto.js
        js는 메소드 오버로딩이 안되서 필요없는 함수 삭제

--- server/src/dto/resultDTO.js
+++ server/src/dto/resultDTO.js
@@ -4,22 +4,13 @@ class ResultDto {
     constructor() {
     }
 
-    // 넘겨줄 데이터가 없을 때
-    makeResult(status, message){
-        let myResult = {
-            status: status,
-            message: message
-        };
-        return myResult;
-    }
     // 넘겨줄 데이터가 있을 때
     makeResult(status, message, result_data) {
-        let myResult = {
+        return {
             status: status,
             message: message,
             result_data: result_data
         };
-        return myResult;
     };
 }
 
