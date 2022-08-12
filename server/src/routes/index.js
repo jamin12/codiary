a62bf81b6484b9fbb638b00d7152a4f4dde6e296
jamin12 <rudals9901@naver.com>
2022-08-12 15:10:22 +0900

    Feat: personalpage
    1. personal 페이지 라우터 생성

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,38 +1,43 @@
-const express = require('express');
-const authRoutes = require('./auth.routes');
-const mainRoutes = require('./main.routes');
-const userRoutes = require('./user.routes');
+const express = require("express");
+const authRoutes = require("./auth.routes");
+const mainRoutes = require("./main.routes");
+const userRoutes = require("./user.routes");
+const personalRoutes = require("./personal.routes");
 
 const router = express.Router();
 
 const defaultRoutes = [
-  {
-    path: '/',
-    route: authRoutes
-  },
-  {
-    path: '/main',
-    route: mainRoutes,
-  },
-  {
-    path: '/user',
-    route: userRoutes
-  },
+	{
+		path: "/",
+		route: authRoutes,
+	},
+	{
+		path: "/main",
+		route: mainRoutes,
+	},
+	{
+		path: "/user",
+		route: userRoutes,
+	},
+	{
+		path: "/personal",
+    route: personalRoutes
+	},
 ];
 
 const devRoutes = [
-  // routes available only in development mode
+	// routes available only in development mode
 ];
 
 defaultRoutes.forEach((route) => {
-  router.use(route.path, route.route);
+	router.use(route.path, route.route);
 });
 
 /* istanbul ignore next */
-if (process.env.NODE_ENV === 'development') {
-  devRoutes.forEach((route) => {
-    router.use(route.path, route.route);
-  });
+if (process.env.NODE_ENV === "development") {
+	devRoutes.forEach((route) => {
+		router.use(route.path, route.route);
+	});
 }
 
 module.exports = router;
