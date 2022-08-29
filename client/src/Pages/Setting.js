e062c0f110db7c7661fd484eb0a69dd5be5fcc66
jamin12 <rudals9901@naver.com>
2022-08-29 19:46:10 +0900

    Style: Setting 페이지 기본틀 css
     - 버튼 누르면 그에 맞는 컴포넌트로 이동하는 기능이 있는 페이지
     - 기본 세팅 페이지 css 완료

--- client/src/Pages/Setting.js
+++ client/src/Pages/Setting.js
@@ -2,53 +2,67 @@ import React from 'react';
 import SearchProfile from '../components/SearchProfile';
 import styled from 'styled-components';
 
+
+
+
+
 const Setting = () => {
 
+  // css
   const Header = styled.div`
+    position: relative;
     width: 100%;
     height: 100%; 
-    background-color: red;
     padding-bottom: 50px;
   `
 
   const ContentsWrap = styled.div`
+    display: flex;
     position: relative;
-    width: 100%;
-    /* background-color: orange; */
-
+    
+    width: 90%;
     height: 100%;
     padding-top: 150px;
     margin: 0 auto;
     margin-top: 100px;
+    background-color: yellow;
 
     div{
-      width: 1536px; //80% 
+      // width: 1536px; //80% 
       margin: 0 auto;
       background-color: pink;
     }
+
+    @media screen and (max-width: 1600px) {
+
+    }
+
   `
 
   const Title = styled.h1`
     font-size: 2.5rem;
     position: absolute;
-    top: 150px;
+    top: 10px;
     left: 50%;
-    width: 12%;
+    width: 12rem;
     text-align: center;
     transform: translateX(-50%);
     border-bottom: 3px solid #c8c8c8;
+    
+    @media screen and (max-width: 1600px) {
+      font-size: 2rem;
+      width: 10rem;
+    }
   `
 
   const Menu = styled.ul`
     position: absolute;
-    background-color: red;
-    top: 230px;
+    // background-color: red;
+    top: 7rem;
     left: 0;
     width: 11rem;
     height: 70vh;
     border-right: 1px solid #000;
-    /* text-align: right; */
-    /* background-color: red; */
 
     li{
       border-bottom: 1px solid #ccc;
@@ -63,12 +77,37 @@ const Setting = () => {
       border-radius: 30px;
       margin: 10px auto;
       background-color: transparent;
+      cursor: pointer;
+
+      &:hover{
+        background-color: #ececec;
+      }
+    }
+
+    @media screen and (max-width: 1600px) {
+      width: 9rem;
+      li button{
+        font-size: 1.2rem;
+      }
     }
   `
 
-  const Contents = styled.div`
+  const Contents = styled.div`  
+    width: 80%;
+    height: 100%;
+    position: absolute;
     background-color: red;
+    right: 0;
+    top: 7rem;
+    width: calc(100% - 13rem);
+
+    @media screen and (max-width: 1600px) {
+      width: calc(100% - 11rem);
+    }
   `
+  // css end
+
+
 
   return (
     <>
@@ -76,7 +115,7 @@ const Setting = () => {
         <SearchProfile/>
       </Header>
     
-      {/* <ContentsWrap>
+      <ContentsWrap>
         <div> 
           <Title>Setting</Title>
           <Menu>
@@ -91,7 +130,7 @@ const Setting = () => {
         </div>
 
 
-      </ContentsWrap> */}
+      </ContentsWrap>
 
     </>
   )
