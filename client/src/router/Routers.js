a39d67935bb77ffa34793a8535d49a0e116f9ac2
leemyou <limhyeon0809@gmail.com>
2022-10-03 12:14:02 +0900

    Style: 라우터 주소 추가

--- client/src/router/Routers.js
+++ client/src/router/Routers.js
@@ -3,6 +3,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import Main from '../Pages/home';
 import Post from '../Pages/Post';
+import Write from '../Pages/WritePost.jsx';
 import Setting from '../Pages/Setting'
 import MyCategory from "../components/SettingMycategory";
 import MyInfo from "../components/SettingMyinfo";
@@ -21,7 +22,8 @@ const Routers = () => {
                     <Route path="category" element={<MyCategory/>}/>
                     <Route path="info" element={<MyInfo/>}/>
                 </Route>
-                
+                <Route path="/write" element={<Write/>}></Route>
+
                 <Route path="/:userId" element={<MyPage/>}></Route>
                 <Route path="/:userId/calender" element={<MyCalender/>}></Route>
                 <Route path="/:userId/presave" element={<Presave/>}></Route>
