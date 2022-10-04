7df249fa3a8c07330f9d9f076b812bfaaa86691c
jamin12 <rudals9901@naver.com>
2022-10-04 20:42:56 +0900

    Feat: post
    1. 포트번호 변경
    2. 포스트 axios 적용

--- client/src/Pages/Post.js
+++ client/src/Pages/Post.js
@@ -1,12 +1,141 @@
 import React from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 import SearchProfile from '../components/SearchProfile';
 import SimilarPost from '../components/SimilarPost';
+import { personal } from '../api/index'
 
-const WritePage = () => {
-  // css
-  const Wrap = styled.div`
+const WritePage = async () => {
+  const getPost = await axios.get(personal.getPersonalPost("test", 1));
+  console.log(getPost);
+
+  // HTML
+  return (
+    <>
+      <SearchProfile />
+
+      <Wrap className="wrap">
+
+        {/* 카테고리창 - sticky 이용 */}
+        <CategoryBox className="sticky-box">
+          {/* <h3>사용자's CODIARY</h3> */}
+          <div className="category">
+            - 카테고리명 -
+            <ul>
+              <li>Category 1qwertyuiasdggjfgfghfghdhtg</li>
+              <li>Category 1</li>
+              <li>Category 1</li>
+              <li>Category 1</li>
+            </ul>
+          </div>
+        </CategoryBox>
+
+
+
+        <ContentWrapBox className='inner-width'>
+          {/* 제목 및 헤더 */}
+          <HeaderBox>
+            <span className="write-data-box">
+              <p className="userName">user1</p>
+              <p className="writeDate">2022.01.01</p>
+            </span>
+            <h1 className="title">TITLES</h1>
+            <span className="cor-del-box">
+              <p>수정</p>
+              /
+              <p>삭제</p>
+            </span>
+            {/* <hr/> */}
+          </HeaderBox>
+
+          {/* 본문내용 */}
+          <ContentBox>
+            <h3 className="sub-title">SubTitle</h3>
+            <p>일단 뭔가를 적어야하니까 적어보겟읍니다.</p>
+            <h3 className="sub-title">What is Lorem Ipsum?</h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+              The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+            <h3 className="sub-title">Why do we use it?</h3>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+          </ContentBox>
+
+          {/* 태그 & 방문자수 */}
+          <TagVisiteBox>
+            <TagBox>
+              <p>TAG</p>
+              <p>머시깽이머시깽이</p>
+            </TagBox>
+
+            <VisiteBox>
+              <div className="total-visite">
+                <ion-icon name="people-outline"></ion-icon>
+                200
+              </div>
+              <div className="today-visite">
+                <ion-icon name="people-outline"></ion-icon>
+                23
+              </div>
+              <div className="like-good">
+                <label for="good"><ion-icon name="heart-outline"></ion-icon></label>
+                <input type="checkbox" id="good" />
+                {/* <ion-icon name="heart"></ion-icon> */}
+                <p>20</p>
+              </div>
+            </VisiteBox>
+          </TagVisiteBox>
+
+          {/* 댓글 */}
+          <CommentBox>
+            <ProfileBox>
+              <img src='' alt=''></img>
+              <p>user2</p>
+            </ProfileBox>
+            <p className="text-box">일단 글자를 한 번 ㅈㄴ 길게 적어봐야할 것 같아서 한 번test문자 넣어봄 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+            <DateBox>
+              <p className="btn-reply">답글쓰기</p>
+              <p className="date">2022.02.02 12:12</p>
+            </DateBox>
+          </CommentBox>
+
+          {/* 덧글 */}
+          <ReplyBox>
+            <ProfileBox>
+              <img src='' alt=''></img>
+              <p>user2</p>
+            </ProfileBox>
+            <p className="text-box">너무 도움이 많이 되었어요~ 감사합니다^^ <br /> 번창하세요!~!~</p>
+            <DateBox>
+              <p className="date">2022.02.02 12:12</p>
+            </DateBox>
+          </ReplyBox>
+
+        </ContentWrapBox>
+
+
+        {/* subtitle창 - sticky 이용 */}
+        <SubTitleBox className="sticky-box">
+          <ul>
+            <li>Subtitle</li>
+            <li>What is Lorem Ipsum?</li>
+            <li>Why do we use it?</li>
+          </ul>
+        </SubTitleBox>
+
+      </Wrap>
+      {/* 비슷한 게시물 props로 태그같은거 보내야함 */}
+      <SimilarPost />
+
+    </>
+
+  )
+
+}
+export default WritePage;
+
+
+// css
+const Wrap = styled.div`
     @media screen and (max-width: 1600px){
       padding-right: 60px;
       padding-left: 60px;
@@ -15,8 +144,8 @@ const WritePage = () => {
       width: 90%;
     }
   `
-    // 카테고리박스
-    const CategoryBox = styled.div`
+// 카테고리박스
+const CategoryBox = styled.div`
       position: relative;
       display: block;
 
@@ -50,7 +179,7 @@ const WritePage = () => {
       }
     `
 
-    const ContentWrapBox = styled.div`
+const ContentWrapBox = styled.div`
       width: 70%;
       margin: 0 auto;
 
@@ -60,7 +189,7 @@ const WritePage = () => {
       }
     `
 
-    const HeaderBox = styled.div`
+const HeaderBox = styled.div`
       display: flex;
       position: relative;
       border-bottom: 2px solid #c8c8c8;
@@ -94,7 +223,7 @@ const WritePage = () => {
       }
     `
 
-    const ContentBox = styled.div`
+const ContentBox = styled.div`
       margin: 0 auto 50px auto;
       .sub-title{
         margin-top: 30px;
@@ -107,7 +236,7 @@ const WritePage = () => {
       }
     `
 
-    const TagVisiteBox = styled.div`
+const TagVisiteBox = styled.div`
       margin: 0 auto 5px auto;
       display: flex;
       justify-content: space-between;
@@ -120,7 +249,7 @@ const WritePage = () => {
       }
     `
 
-    const TagBox = styled.div`
+const TagBox = styled.div`
       display: flex;
       max-width: 600px;
       p{
@@ -132,7 +261,7 @@ const WritePage = () => {
       }
     `
 
-    const VisiteBox = styled.div`
+const VisiteBox = styled.div`
       display: flex;
       width: 170px;
       justify-content: space-between;
@@ -174,7 +303,7 @@ const WritePage = () => {
       }
     `
 
-    const CommentBox = styled.div`
+const CommentBox = styled.div`
     margin-top: 30px;
     /* 댓글 창 */
     .text-box{
@@ -192,7 +321,7 @@ const WritePage = () => {
     }
     `
 
-    const ReplyBox = styled.div`
+const ReplyBox = styled.div`
       /* width: 60%; */
       margin-top: 15px;
       margin-left: 40px;
@@ -211,8 +340,8 @@ const WritePage = () => {
       }
     `
 
-    /* 날짜박스 */
-    const DateBox = styled.div`
+/* 날짜박스 */
+const DateBox = styled.div`
       display: flex;
       width: 90%;
       margin: 0 auto;
@@ -228,7 +357,7 @@ const WritePage = () => {
       }
     `
 
-    const ProfileBox = styled.div`
+const ProfileBox = styled.div`
       display: flex;
       padding-bottom: 5px;
       p{
@@ -253,8 +382,8 @@ const WritePage = () => {
       }
     `
 
-    // 서브타이틀 박스
-    const SubTitleBox = styled.div`
+// 서브타이틀 박스
+const SubTitleBox = styled.div`
       width: 13%;
       z-index: 999;
       right: 0;
@@ -285,129 +414,3 @@ const WritePage = () => {
         margin: 0 auto;
       }
     `
-
-
-
-
-    // HTML
-    return (
-        <>
-            <SearchProfile/>
-
-            <Wrap className="wrap">
-
-              {/* 카테고리창 - sticky 이용 */}
-              <CategoryBox className="sticky-box">
-                {/* <h3>사용자's CODIARY</h3> */}
-                <div className="category">
-                - 카테고리명 -
-                  <ul>
-                    <li>Category 1qwertyuiasdggjfgfghfghdhtg</li>
-                    <li>Category 1</li>
-                    <li>Category 1</li>
-                    <li>Category 1</li>
-                  </ul>
-                </div>
-              </CategoryBox>
-
-              
-
-              <ContentWrapBox className='inner-width'>
-                {/* 제목 및 헤더 */}
-                <HeaderBox>
-                  <span className="write-data-box">
-                    <p className="userName">user1</p>
-                    <p className="writeDate">2022.01.01</p>
-                  </span>
-                  <h1 className="title">TITLES</h1>
-                  <span className="cor-del-box">
-                    <p>수정</p>
-                    /
-                    <p>삭제</p>
-                  </span>
-                  {/* <hr/> */}
-                </HeaderBox>
-
-                {/* 본문내용 */}
-                <ContentBox>
-                  <h3 className="sub-title">SubTitle</h3>
-                  <p>일단 뭔가를 적어야하니까 적어보겟읍니다.</p>
-                  <h3 className="sub-title">What is Lorem Ipsum?</h3>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-                    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-                  <h3 className="sub-title">Why do we use it?</h3>
-                  <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-                </ContentBox>
-
-                {/* 태그 & 방문자수 */}
-                <TagVisiteBox>
-                  <TagBox>
-                    <p>TAG</p>
-                    <p>머시깽이머시깽이</p>
-                  </TagBox>
-
-                  <VisiteBox>
-                    <div className="total-visite">
-                      <ion-icon name="people-outline"></ion-icon>
-                      200
-                    </div>
-                    <div className="today-visite">
-                      <ion-icon name="people-outline"></ion-icon>
-                      23
-                    </div>
-                    <div className="like-good">
-                      <label for="good"><ion-icon name="heart-outline"></ion-icon></label>
-                      <input type="checkbox" id="good"/>
-                        {/* <ion-icon name="heart"></ion-icon> */}
-                      <p>20</p>
-                    </div>
-                  </VisiteBox>
-                </TagVisiteBox>
-
-                {/* 댓글 */}
-                <CommentBox>
-                  <ProfileBox>
-                    <img src='' alt=''></img>
-                    <p>user2</p>
-                  </ProfileBox>
-                  <p className="text-box">일단 글자를 한 번 ㅈㄴ 길게 적어봐야할 것 같아서 한 번test문자 넣어봄 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-                  <DateBox>
-                    <p className="btn-reply">답글쓰기</p>
-                    <p className="date">2022.02.02 12:12</p>
-                  </DateBox>
-                </CommentBox>
-
-                {/* 덧글 */}
-                <ReplyBox>
-                  <ProfileBox>
-                    <img src='' alt=''></img>
-                    <p>user2</p>
-                  </ProfileBox>
-                  <p className="text-box">너무 도움이 많이 되었어요~ 감사합니다^^ <br/> 번창하세요!~!~</p>
-                  <DateBox>
-                    <p className="date">2022.02.02 12:12</p>
-                  </DateBox>
-                </ReplyBox>
-
-              </ContentWrapBox>
-
-
-              {/* subtitle창 - sticky 이용 */}
-              <SubTitleBox className="sticky-box">
-                <ul>
-                  <li>Subtitle</li>
-                  <li>What is Lorem Ipsum?</li>
-                  <li>Why do we use it?</li>
-                </ul>
-              </SubTitleBox>
-
-            </Wrap>
-            {/* 비슷한 게시물 props로 태그같은거 보내야함 */}
-            <SimilarPost/>
-
-        </>
-
-    )
-
-}
-export default WritePage;
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,5 +2,5 @@ import auth from "./authApi";
 import personal from "./personalpageApi";
 import user from "./userApi";
 import manage from "./manage";
-const baseUrl = "127.0.0.1:3000";
+const baseUrl = "127.0.0.1:3012";
 export { baseUrl, auth, personal, user, manage };
