7710972dbdf48689b8ac26bf969097e9635312b7
leemyou <limhyeon0809@gmail.com>
2022-10-04 17:19:21 +0900

    Feat: editor 수정중
    - 익스텐션이 추가 안되고 있음
    - 백틱 3개 이후에 엔터가 안눌려지는 오류 해결중
    - code highlighter 패키지 추가

--- client/src/Pages/WritePost.jsx
+++ client/src/Pages/WritePost.jsx
@@ -10,6 +10,11 @@ import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-sy
 import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
 // 한국어 플러그인
 import '@toast-ui/editor/dist/i18n/ko-kr';
+// code lightlite 플러그인
+import 'prismjs/themes/prism.css';
+import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
+import Prism from 'prismjs';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
 
 import SearchProfile from "../components/SearchProfile";
 import MessageBox from "../components/MessageBox";
@@ -26,7 +31,7 @@ const WriteWrap = styled.div`
 const WriteBox = styled.div`
   width: 100%;
   height: 100%;
-  background-color: var(--gray200);
+  background-color: #fff;
 `
 const BtnWrap = styled.div`
   position: fixed;
@@ -57,22 +62,22 @@ const BtnWrap = styled.div`
 const WritePost = () => {
 
   const [modal, setModal] = useState(false);
-
+  const [dataHtml, setHtml] = useState('');
+  const [dataMd, setMd] = useState('');
 
   const editorRef = useRef();
   // editorRef.current().getInstance()의 형식으로 에디터의 설정값들을 갖고 올 수 있음
 
-  // 각각 html, 마크다운 형태로 가져올 수 있음
-  const toHTML = editorRef.current?.getInstance().getHTML()
-  const toMARKDOWN = editorRef.current?.getInstance().getMarkdown()
-
-
+  const onChangeEditor = () => {
+    setHtml(editorRef.current.getInstance().getHTML());
+    setMd(editorRef.current.getInstance().getMarkdown());
+  }
   const onClickSave = () => {
-    console.log(toHTML)
-    console.log(toMARKDOWN)
+    console.log(dataHtml)
+    console.log(dataMd)
     // 서버에 toHTML과 toMARKDOWN 전송
     setModal(true);
-    // console.log(modal)
+    console.log(modal)
   }
   const onClickPresave = () => {
     // 서버에 임시저장 파일로 전송
@@ -90,15 +95,16 @@ const WritePost = () => {
         <WriteBox>
             <Editor
               ref={editorRef} // DOM 선택용 useRef
-              initialValue = 'Hello World!' // 임시 내용
+              onChange={onChangeEditor}
+              initialValue = '' // 임시 내용
               previewStyle='vertical' // 미리보기 스타일
               height='100%'
               className='codeMirror-editor'
               initialEditType='markdown'  // 처음 언어 설정을 마크다운으로 설정
-              useCommandShortcut={false}  // 키보드 입력 컨트롤 방지
+              useCommandShortcut={true}  // 키보드 입력 컨트롤 방지
               hideModeSwitch= {true}// 한 가지 타입(마크다운)만 사용하고싶으면 설정
-              plugins={[colorSyntax]}
-              language="ko-KR"
+              plugins={[colorSyntax, [codeSyntaxHighlight, { highlighter: Prism }]]}
+              language="ko-KR"  // 초기 언어 세팅: 한글
               toolbarItems={[
                 ['heading', 'bold', 'italic', 'strike'],
                 ['hr', 'quote'],
@@ -108,14 +114,38 @@ const WritePost = () => {
                 [{
                   name:'compiler',
                   tooltip: '코드 컴파일러',
-                  command: '!~~!',
-                  text: '⚙️',
+                  command: 'htmlBlock',
+                  text: 'CC',
                   className: 'btn-code-compiler',
                   styled: {
                     fontSize: '20px'
                   },
                   state: 'compiler'
-                }, 'scrollSync']
+                }, 'scrollSync'
+              ]
+              ]}
+              // customHTMLRenderer={{
+              //   htmlBlock:{
+              //     compiler(node){
+              //       return[
+              //         { type: 'openTag', tagName: 'div', outerNewLine: true },
+              //         { type: 'html', content: node.childrenHTML },
+              //         { type: 'closeTag', tagName: 'div', outerNewLine: true }
+              //       ]
+              //     }
+              //   }
+              // }}
+              exts={[
+                {name: 'chart',
+                minWidth: 100,
+                maxWidth: 600,
+                minHeight: 100,
+                maxHeight: 300},
+                'scrollSync',
+                'colorSyntax',
+                'uml',
+                'mark',
+                'table'
               ]}
             />
         </WriteBox>
