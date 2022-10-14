9867d536c359f1f019a319470b9de003f8421892
leemyou <limhyeon0809@gmail.com>
2022-10-14 20:14:52 +0900

    Style : 타이틀 입력창 추가

--- client/src/Pages/WritePost.jsx
+++ client/src/Pages/WritePost.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import styled from "styled-components";
 
 // TOAST UI Editor
@@ -17,102 +17,61 @@ import Prism from "prismjs";
 import codeSyntaxHighlight from "@toast-ui/editor-plugin-code-syntax-highlight";
 
 import SearchProfile from "../components/SearchProfile";
-import MessageBox from "../components/MessageBox";
-import { useState } from "react";
 import axios from "axios";
 import { personal } from "../api/index";
-import { useEffect } from "react";
 
-//css
+import Modal from '../components/SaveModal'
 
-const WriteWrap = styled.div`
-	width: 100%;
-	height: 100%;
-	background-color: var(--gray50);
-`;
-const WriteBox = styled.div`
-	width: 100%;
-	height: 100%;
-	background-color: #fff;
-`;
-const BtnWrap = styled.div`
-	position: fixed;
-	bottom: 30px;
-	right: 30px;
-	width: 250px;
-	height: 30px;
-	display: flex;
-	justify-content: space-between;
 
-	button {
-		width: 45%;
-		height: 100%;
-		border: none;
-		background-color: var(--gray200);
-		color: var(--gray900);
-		transition: 0.2s;
-		box-shadow: 0px 5px 10px 0px var(--gray400);
-		:hover {
-			background-color: var(--gray600);
-			color: var(--gray50);
-		}
-	}
-`;
-
+/**
+ * post 쓰는 부분
+ */
 const WritePost = () => {
-	const [modal, setModal] = useState(false);
+	const [modalShow, setModalShow] = React.useState(false);
 	const [dataHtml, setHtml] = useState("");
 	const [dataMd, setMd] = useState("");
+	const [title, setTitle] = useState("");
 
 	const editorRef = useRef();
 	// editorRef.current().getInstance()의 형식으로 에디터의 설정값들을 갖고 올 수 있음
+
+	const titleChange = (e) => {
+		setTitle(e.target.value);
+		console.log(title);
+	}
 
 	const onChangeEditor = () => {
 		setHtml(editorRef.current.getInstance().getHTML());
 		setMd(editorRef.current.getInstance().getMarkdown());
 	};
+
+
 	/**
-	 * 포스트 저장
+	 * 모달창 open
 	 */
-	const onClickSave = async () => {
-		// TODO: body부분 값 변경
-		await axios.post(
-			personal.createPersonalPost(),
-			{
-				post: {
-					post_title: "테스으 생성",
-					post_body_md: "null",
-					post_body_html: "null",
-					post_txt: "qwer12v",
-					category_id: 2,
-				},
-				tag: {
-					tag_name: ["테스트 태그", "리스트 테스트2"],
-				},
-			},
-			{
-				withCredentials: true,
-				headers: { "Content-Type": `application/json` },
-			}
-		);
-		console.log(dataHtml);
-		console.log(dataMd);
-		// 서버에 toHTML과 toMARKDOWN 전송
-		setModal(true);
-		console.log(modal);
-	};
+	const onClickModalOpen = () => {
+		if (title === null) {
+			alert("제목을 먼저 입력해주세요")
+		} else {
+			setModalShow(true);
+		}
+	}
+
 
 	/**
 	 * 포스트 임시 저장
 	 */
 	const onClickPresave = async () => {
+		if (title !== null) {
+			alert("제목을 먼저 입력해주세요")
+		}
 		// TODO: body부분 값 변경
 		await axios.post(
 			personal.createPersonalTmpPost(),
 			{
-				tmppost_title: "create test title1",
-				tmppost_body_md: "null",
-				tmppost_body_html: "null",
+				tmppost_title: title,
+				tmppost_body_md: dataMd,
+				tmppost_body_html: dataHtml,
 				tmppost_txt: "create test txt1",
 			},
 			{
@@ -128,6 +87,9 @@ const WritePost = () => {
 			<SearchProfile />
 
 			<WriteWrap>
+				<Title>
+					<input type="text" onChange={titleChange} value={title} placeholder="제목을 입력하세요"></input>
+				</Title>
 				<WriteBox>
 					<Editor
 						ref={editorRef} // DOM 선택용 useRef
@@ -197,15 +159,79 @@ const WritePost = () => {
 					<button type="button" onClick={onClickPresave}>
 						임시저장
 					</button>
-					<button type="submit" onClick={onClickSave}>
+					<button type="submit" onClick={onClickModalOpen}>
 						저장
 					</button>
 				</BtnWrap>
 			</WriteWrap>
 
-			{/* messageBox */}
-			<MessageBox visible="true" />
+			{/* 모달창 */}
+			<Modal
+				show={modalShow}
+				onHide={() => setModalShow(false)}
+				title = {title}
+				dataMd = {dataMd}
+				dataHtml = {dataHtml}
+				dataTxt = {dataHtml.innerText}
+			/>
 		</div>
 	);
 };
 export default WritePost;
+
+
+
+//css
+
+const WriteWrap = styled.div`
+	width: 100%;
+	height: 100%;
+	background-color: var(--gray50);
+`;
+const WriteBox = styled.div`
+	width: 100%;
+	height: 100%;
+	background-color: #fff;
+`;
+const BtnWrap = styled.div`
+	position: fixed;
+	bottom: 30px;
+	right: 30px;
+	width: 250px;
+	height: 30px;
+	display: flex;
+	justify-content: space-between;
+
+	button {
+		width: 45%;
+		height: 100%;
+		border: none;
+		background-color: var(--gray200);
+		color: var(--gray900);
+		transition: 0.2s;
+		box-shadow: 0px 5px 10px 0px var(--gray400);
+		:hover {
+			background-color: var(--gray600);
+			color: var(--gray50);
+		}
+	}
+`;
+
+const Title = styled.div`
+	width: 100%;
+	height: 50px;
+	display: flex;
+	justify-content: center;
+
+	input{
+		background-color: var(--gray50);
+		border: none;
+		width: 90%;
+		height: 100%;
+		font-size: 20px;
+		font-weight: bold;
+	}
+	input:focus{
+		outline: none;
+	}
+`
