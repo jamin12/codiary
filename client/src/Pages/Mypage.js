a43181f2e88888e170f835dee38ead08f1d4fa68
leemyou <limhyeon0809@gmail.com>
2022-10-28 23:28:19 +0900

    Style: 마이페이지 캐러셀 진행중

--- client/src/Pages/Mypage.js
+++ client/src/Pages/Mypage.js
@@ -1,97 +1,278 @@
 import React, { useEffect, useState } from "react";
-import "../css/MypageStyle.css";
+import "../css/reset.css";
+import styled from "styled-components";
 import { useParams } from "react-router-dom";
+
+import Carousel from '../components/CarouselSlick';
 
 import SearchProfile from "../components/SearchProfile";
 import { personal } from "../api/index";
 import axios from "axios";
 
 const Mypage = () => {
-	const { userId } = useParams();
+  const { userId } = useParams();
 
-	const [Nickname] = useState("Emyo");
-	const [category, setCategory] = useState({});
-	const [posts, setPosts] = useState({});
+  const [Nickname] = useState("Emyo");
+  const [category, setCategory] = useState([
+    {
+      "category_id": 1,
+      "sub_category_id": null,
+      "category_name": "알고리즘"
+    },
+  ]);
+  const [posts, setPosts] = useState([
+    {
+      "post_id": 25,
+      "post_title": "테스으 생성",
+      "post_body_md": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      "post_body_html": "<p><h1>Lorem Ipsum</h1> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>",
+      "post_txt": "qwer12v",
+      "created_at": "2022-08-29 03:52",
+      "updated_at": "2022-08-29 19:45",
+      "users": {
+          "user_email": "rudals951004@gmail.com",
+          "user_detail": {
+              "user_name": "min ja",
+              "user_unique_id": "test",
+              "user_nickname": "",
+              "user_img": "이미지가 없다링"
+          }
+      }
+    },
+    {
+      "post_id": 26,
+      "post_title": "이묘이묘",
+      "post_body_md": "123",
+      "post_body_html": "<p>123</p>",
+      "post_txt": "qwer12v",
+      "created_at": "2022-08-29 03:52",
+      "updated_at": "2022-08-29 19:45",
+      "users": {
+          "user_email": "rudals951004@gmail.com",
+          "user_detail": {
+              "user_name": "min ja",
+              "user_unique_id": "test",
+              "user_nickname": "코딩하는사람",
+              "user_img": "https://images.unsplash.com/photo-1666616328135-ad1b7e62a25c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=1000&q=60"
+          }
+      }
+    },
+    {
+      "post_id": 27,
+      "post_title": "진자 존나",
+      "post_body_md": "null",
+      "post_body_html": "null",
+      "post_txt": "qwer12v",
+      "created_at": "2022-08-29 03:52",
+      "updated_at": "2022-08-29 19:45",
+      "users": {
+          "user_email": "rudals951004@gmail.com",
+          "user_detail": {
+              "user_name": "min ja",
+              "user_unique_id": "test",
+              "user_nickname": "이묘ㅛ",
+              "user_img": "https://images.unsplash.com/photo-1666526320369-a1e3fcd69253?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=1000&q=60"
+          }
+      }
+    },
+    {
+      "post_id": 28,
+      "post_title": "귀찮아",
+      "post_body_md": "null",
+      "post_body_html": "null",
+      "post_txt": "qwer12v",
+      "created_at": "2022-08-29 03:52",
+      "updated_at": "2022-08-29 19:45",
+      "users": {
+          "user_email": "rudals951004@gmail.com",
+          "user_detail": {
+              "user_name": "min ja",
+              "user_unique_id": "test",
+              "user_nickname": "이묘",
+              "user_img": "https://images.unsplash.com/photo-1666473574975-fd909b53dd4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=1000&q=60"
+          }
+      }
+    },
+  ]);
 
-	/**
-	 *  사용자 카테고리 목룍 조회
-	 */
-	useEffect(() => {
-		const getCategoryFun = async () => {
-			const getCategory = await axios.get(
-				personal.getPersonalCategory(userId)
-			);
-			setCategory(getCategory.data.result_data);
-		};
-		getCategoryFun();
-	}, [userId]);
+  /**
+   *  사용자 카테고리 목록 조회
+   */
+  useEffect(() => {
+    const getCategoryFun = async () => {
+      const getCategory = await axios.get(
+        personal.getPersonalCategory(userId)
+      );
+      setCategory(getCategory.data.result_data);
+    };
+    getCategoryFun();
+  }, [userId]);
 
-	/**
-	 *  사용자 카테고리별 포스트 목록 조회
-	 */
-	useEffect(() => {
-		const getPostsFun = async () => {
-			const getPost = await axios.get(
+  /**
+   *  사용자 카테고리별 포스트 목록 조회
+   */
+  useEffect(() => {
+    const getPostsFun = async () => {
+      const getPost = await axios.get(
         //TODO: 카테고리 아이디 파라미터로 가져와야함
-				personal.getPsersonalPosts(userId, 2),
-				{
+        personal.getPsersonalPosts(userId, 2),
+        {
           //TODO: queryParams로 가져와야함
-					params: { offset: 1, limit: 4 },
-				}
-			);
-			setPosts(getPost.data.result_data);
-		};
-		getPostsFun();
-	}, [userId]);
+          params: { offset: 1, limit: 4 },
+        }
+      );
+      setPosts(getPost.data.result_data);
+    };
+    getPostsFun();
+  }, [userId]);
 
-	console.log(category);
-	console.log(posts);
+  console.log(category);
+  console.log(posts);
 
-	return (
-		<div className="Mypage-wrap">
-			<SearchProfile />
+  return (
+    <MainWrap>
+      <SearchProfile />
 
-			{/* 가운데 홈 글씨 */}
-			<div className="mypage-main-txt">
-				<h3>{Nickname}'s</h3>
-				<h1>CODIARY</h1>
-			</div>
+      <Contents>
+        {/* 가운데 홈 글씨 */}
+        <div className="mypage-main-txt">
+          <h3>{Nickname}'s</h3>
+          <h1>CODIARY</h1>
+        </div>
 
-			<div></div>
+        <div></div>
 
-			<div className="folder-wrap">
-				<div className="folder">
-					<h3>전체보기</h3>
-					<ion-icon name="chevron-down-outline"></ion-icon>
-				</div>
+        <Folders>
+          <div className="folder full-view">
+            전체보기
+          </div>
 
-				<div className="folder">
-					<h3>전체보기</h3>
-					<ion-icon name="chevron-down-outline"></ion-icon>
-				</div>
-				<div className="folder">
-					<h3>전체보기</h3>
-					<ion-icon name="chevron-down-outline"></ion-icon>
-				</div>
-				<div className="folder">
-					<h3>전체보기</h3>
-					<ion-icon name="chevron-down-outline"></ion-icon>
-				</div>
-				<div className="folder">
-					<h3>전체보기</h3>
-					<ion-icon name="chevron-down-outline"></ion-icon>
-				</div>
-				<div className="folder">
-					<h3>전체보기</h3>
-					<ion-icon name="chevron-down-outline"></ion-icon>
-				</div>
-				<div className="folder">
-					<h3>전체보기</h3>
-					<ion-icon name="chevron-down-outline"></ion-icon>
-				</div>
-			</div>
-		</div>
-	);
+          <div className="category-folder-box">
+            {/* TODO(이묘): 서버에서 받아온 카테고리를 map 함수로 뿌려줘야함 */}
+            {
+              category.map((category) => {
+                return(
+                  <div className="folder">
+                  <span>{category.category_name}</span>
+                  <ion-icon name="chevron-down-outline"></ion-icon>
+                </div>
+                )
+              })
+            }
+          </div>
+        </Folders>
+
+        <CarouselWrap>
+          <Carousel
+            posts={posts}
+            dots={false}
+            slidesToShow={3}
+            vertical={true}
+            verticalSwiping={true}
+            centerMode={true}
+            centerPadding={'60px'}
+            className={'center'}
+          />
+        </CarouselWrap>
+
+
+      </Contents>
+
+    </MainWrap>
+  );
 };
 
 export default Mypage;
+
+
+
+const MainWrap = styled.div`
+  position: relative;
+  height: 100vh;
+  width: 100%;
+  .mypage-main-txt{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    cursor: pointer;
+  }
+`
+
+const Contents = styled.div`
+  position: relative;
+  width: 88%;
+  max-width: 1500px;
+  height: 80%;
+  margin: 0 auto;
+`
+
+const Folders = styled.div`
+  position: absolute;
+  width: 35%;
+  /* height: 50%; */
+  top: 50%;
+  right: 0px;
+  transform: translateY(-50%);
+
+  .full-view{
+    background-color: var(--gray200);
+    width: 100%;
+    height: 45px;
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+    padding: 0 0 0 10px;
+    font-size: 1.1rem;
+    font-weight: 500;
+    cursor: pointer;
+
+    :hover{
+      background-color: var(--gray300);
+    }
+  }
+  .folder{
+    display: flex;
+    align-items: center;
+    border-radius: 13px;
+    cursor: pointer;
+    h3{
+      font-size:1.2rem;
+    }
+    ion-icon{
+      font-size: 1.3rem;
+    }
+  }
+  .category-folder-box{
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 10px;
+
+    .folder{
+      background-color: var(--gray100);
+      height: 40px;
+      box-sizing: border-box;
+      padding: 0 0 0 10px;
+      /* margin: 5px 0; */
+      :hover{
+        background-color: var(--gray200);
+      }
+      span{
+        font-size: 1.1rem;
+        font-weight: 500;
+      }
+
+      :first-child, :nth-child(2){
+        margin-top: 10px;
+      }
+    }
+  }
+
+`
+const CarouselWrap = styled.div`
+  width: 35%;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 0;
+`
--- client/src/components/CarouselSlick.jsx
+++ client/src/components/CarouselSlick.jsx
@@ -2,53 +2,141 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import styled from "styled-components";
 
-export default class MultipleItems extends Component {
+export default class Carousel extends Component {
   render() {
-
-    const visitLikeRecords = this.props;
+    const posts = this.props.posts  // props로 post들을 받아옴
+    console.log(posts)
 
     const settings = {
-      dots: true,
+      className: this.props.className,
+      centerMode: this.props.centerMode,
+      dots: this.props.dots,
       infinite: true,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 3
+      slidesToShow: this.props.slidesToShow,
+      slidesToScroll: 1,
+      vertical: this.props.vertical,
+      verticalSwiping: this.props.verticalSwiping,
+      centerPadding: this.props.centerPadding,
+
+
+      beforeChange: function (currentSlide, nextSlide) {
+        console.log("before change", currentSlide, nextSlide);
+      },
+      afterChange: function (currentSlide) {
+        console.log("after change", currentSlide);
+      }
     };
+
     return (
       <div>
         <Slider {...settings}>
-            {
-                
-            }
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-          <div>
-            <h3>7</h3>
-          </div>
-          <div>
-            <h3>8</h3>
-          </div>
-          <div>
-            <h3>9</h3>
-          </div>
+          {
+            posts.map((post) => {
+              return (
+                <>
+                  <Post>
+                    <div className="text-wrap">
+                      <h3 className="post-title">{post.post_title}</h3>
+                      <div className="user-info-box">
+                        <img src={post.users.user_detail.user_img} alt="사용자이미지" />
+                        <span>{post.users.user_detail.user_nickname}</span>
+                      </div>
+                      <p className="post-date">{post.updated_at}</p>
+                    </div>
+
+                    {/* TODO(이묘): 대표 이미지가 있는지 없는지 체크하고 대표 이미지가 있으면 이미지를, 없으면 post_body_md가 나타나도록 수정
+                      현재는 그냥 post_body_md만 나타나도록 함.
+                    */}
+                    <div className="post-img-wrap">
+                      {
+                        // TODO(이묘): post 이미지가 있는지 없는지 확인하는 함수 구현 필요
+                        post.img ? <img src={post.img} alt="게시물 대표 이미지"/> : <p>{post.post_body_md}</p>
+                      }
+                    </div>
+                  </Post>
+                </>
+              )
+            })
+          }
         </Slider>
       </div>
     );
   }
 }
+
+const Post = styled.div`
+  width: 100%;
+  height: 150px;
+  margin: 5px 0;
+  border-radius: 15px;
+  background-color: var(--gray50);
+  padding: 5px;
+  box-sizing: border-box;
+  display: flex;
+
+  .text-wrap{
+    width: 50%;
+    height: 100%;
+    display: block;
+    position: relative;
+
+    .post-title{
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      margin-top: 10px;
+      margin-left: 10px;
+      font-size: 1.5rem;
+    }
+
+    .user-info-box{
+      display: flex;
+      align-items: center;
+      margin-top: 10px;
+      margin-left: 5px;
+      img{
+        width: 30px;
+        height: 30px;
+        border-radius: 50%;
+        object-fit: cover;
+        object-position: center;
+      }
+      span{
+        font-size: 0.9rem;
+      }
+    }
+    .post-date{
+      color: var(--gray600);
+      position: absolute;
+      bottom: -10px;
+      left: 10px;
+    }
+  }
+  .post-img-wrap{
+    width: 50%;
+    height: 100%;
+    float: right;
+    background-color: var(--gray100);
+    border-radius: 15px;
+    img{
+      width: 100%;
+      height: 100%;
+      border-radius: 15px;
+      display: block;
+      object-fit: cover;
+      object-position: center;
+    }
+    p{
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      /* text-align: left; */
+      word-break: break-word;
+      -webkit-line-clamp: 4;
+      -webkit-box-orient: vertical;
+      color: var(--gray600);
+    }
+  }
+`
