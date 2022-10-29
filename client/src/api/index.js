e4508930e7075d93b0401d26ea1db95b4967ebaa
jamin12 <rudals9901@naver.com>
2022-10-29 16:22:14 +0900

    Feat: front_datainsert
    1. 통계페이지 데이터 넣기

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,5 +3,6 @@ import personal from "./personalpageApi";
 import user from "./userApi";
 import manage from "./manage";
 import main from "./mainpageApi";
+import measurement from "./measurementApi";
 var baseUrl = "http://127.0.0.1:3000";
-export { baseUrl, auth, personal, user, manage, main };
+export { baseUrl, auth, personal, user, manage, main, measurement };
--- client/src/components/SaveModal.jsx
+++ client/src/components/SaveModal.jsx
@@ -7,7 +7,7 @@ import '../css/reset.css';
 // bootstrap
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function OptionModal(props) {
   const [tagInput, setTagInput] = useState('');
