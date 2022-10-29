6619ecd39f710fd862bbaefd5fc492e015d7f896
Hyohyeon Lim <limhyeon0809@gmail.com>
2022-10-29 19:31:50 +0900

    Style: header 검색 input text를 버튼으로 수정

--- client/src/components/SearchProfile.js
+++ client/src/components/SearchProfile.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import styled from "styled-components";
-import '../css/reset.css';
-import { devices } from '../css/DeviceSize';
-import { Link } from 'react-router-dom';
-import Login from './Login';
+import "../css/reset.css";
+import { devices } from "../css/DeviceSize";
+import { Link } from "react-router-dom";
+import Login from "./Login";
 
 // 스타일 설정
 // 1680*900 기준 작성
@@ -12,7 +12,7 @@ const Main = styled.div`
   height: 120px;
   position: relative;
   z-index: 900;
-`
+`;
 const Wrap = styled.div`
   position: absolute;
   display: flex;
@@ -20,7 +20,7 @@ const Wrap = styled.div`
   left: 50%;
   transform: translateX(-50%);
 
-  width: 1536px; //80% 
+  width: 1536px; //80%
   height: 50px;
   align-items: center;
   justify-content: space-between;
@@ -29,70 +29,34 @@ const Wrap = styled.div`
   @media screen and (max-width: 1600px) {
     width: 95vw;
   }
-
-`
+`;
 const SearchWrap = styled.div`
   position: relative;
   display: flex;
-`
-const SearchBox = styled.div`
-  position: relative;
-  display: flex;
-  background-color: #E8E8E8;
-  border-radius: 30px;
-  width: 250px;
-  overflow: hidden;
+  align-items: center;
+  justify-content: space-between;
+`;
 
-  ion-icon{ //돋보기
-    position: absolute;
-    width: 25px;
-    height: 100%;
-    padding: 0 10px 0 5px;
-    right: 0;
-
-    cursor: pointer;
-  }
-
-  @media ${devices.laptopL} {
-    width: 220px;
-
-    ion-icon{
-    width: 20px;
-    padding: 0 13px 0 5px;
-
-    }
-  }
-`
-const Search = styled.input`
-  position: absolute;
-  width: calc(100% - 60px);
-  height: 100%;
-  margin-left: 15px;
-  background-color: transparent;
-  border: none;
-  :focus{
-    outline: none;
-  }
-`
 const Profile = styled.div`
   width: 100px;
   height: 40px;
-  margin-left: 30px;
+  margin-left: 25px;
 
   display: flex;
   justify-content: space-between;
 
-  .userBox{
+  .userBox {
     position: relative;
     width: 40px;
     height: 40px;
 
     // 현재 이미지 안들어감 해결 해야함.
-    .imgBox{
+    .imgBox {
       position: relative;
       min-width: 40px;
       height: 40px;
-      /* background-image: url(${(props) => props.img || '../IMG/KAKAO.png.png'}); */
+      /* background-image: url(${(props) =>
+        props.img || "../IMG/KAKAO.png.png"}); */
       background-color: orange;
       background-size: cover;
       background-repeat: no-repeat;
@@ -103,7 +67,7 @@ const Profile = styled.div`
     }
   }
 
-  .menuToggleOFF{
+  .menuToggleOFF {
     position: relative;
     width: 40px;
     height: 40px;
@@ -112,8 +76,8 @@ const Profile = styled.div`
     justify-content: center;
     align-items: center;
 
-    ::before{
-      content: '';
+    ::before {
+      content: "";
       position: absolute;
       width: 32px;
       height: 2px;
@@ -121,9 +85,9 @@ const Profile = styled.div`
       transform: translateY(-10px);
       box-shadow: 0 10px var(--gray600);
       transition: 0.5s;
-    }    
-    ::after{
-      content: '';
+    }
+    ::after {
+      content: "";
       position: absolute;
       width: 32px;
       height: 2px;
@@ -133,7 +97,7 @@ const Profile = styled.div`
     }
   }
 
-  .menuToggleON{
+  .menuToggleON {
     position: relative;
     width: 40px;
     height: 40px;
@@ -142,8 +106,8 @@ const Profile = styled.div`
     justify-content: center;
     align-items: center;
 
-    ::before{
-      content: '';
+    ::before {
+      content: "";
       position: absolute;
       width: 32px;
       height: 2px;
@@ -151,9 +115,9 @@ const Profile = styled.div`
       transform: translateY(0px) rotate(45deg);
       box-shadow: 0 0px var(--gray600);
       transition: 0.5s;
-    }    
-    ::after{
-      content: '';
+    }
+    ::after {
+      content: "";
       position: absolute;
       width: 32px;
       height: 2px;
@@ -162,32 +126,32 @@ const Profile = styled.div`
       transition: 0.5s;
     }
   }
-`
+`;
 const Menu = styled.div`
   position: absolute;
   right: 0;
   top: 4.5rem;
-  .menuOFF{
+  .menuOFF {
     transition: 0.5s;
     width: 200px;
     height: 0;
-    .tagP{
+    .tagP {
       display: none;
     }
   }
-  .menuON{
+  .menuON {
     position: relative;
     transition: 0.5s;
     display: inline-block;
     width: 200px;
     height: 250px;
     background-color: var(--gray50);
-    box-shadow: 0 25px 35px rgba(0,0,0,0.1);
+    box-shadow: 0 25px 35px rgba(0, 0, 0, 0.1);
     box-sizing: border-box;
     padding: 20px;
 
-    ::before{
-      content: '';
+    ::before {
+      content: "";
       width: 30px;
       height: 30px;
       position: absolute;
@@ -198,120 +162,123 @@ const Menu = styled.div`
       z-index: -1;
     }
 
-    .tagP{
+    .tagP {
       display: block;
       font-size: 1.1rem;
       width: 100%;
       margin-bottom: 1px;
       padding: 3px 0;
       cursor: pointer;
-      :hover{
+      :hover {
         background-color: var(--gray100);
       }
     }
-    .logout{
+    .logout {
       position: absolute;
       color: red;
       bottom: 20px;
       left: 20px;
       width: 160px;
-      :hover{
-        color: #AE1414;
+      :hover {
+        color: #ae1414;
       }
     }
   }
-`
+`;
 const GotoHome = styled.h1`
   font-size: 2.5rem;
   color: var(--gray900);
 
-  span{
+  span {
     color: var(--gray900);
     text-decoration: none;
   }
 
-  @media ${devices.laptopL}{
+  @media ${devices.laptopL} {
     font-size: 2rem;
+  }
+`;
+const BtnSearch = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 7px;
+
+  ion-icon {
+    width: 30px;
+    height: 30px;
   }
 `
 
-
-
-
-
-
-
 // 회원 정보를 받아와서 프로필 사진을 불러와야 함
 const SearchProfile = () => {
-
   const [isOpen, setMenu] = useState(false);
-  const [searchWord, setSearch] = useState('');
+  const [searchWord, setSearch] = useState("");
   const [loginOpen, setLogin] = useState(false);
 
   const toggleMenu = () => {
-      setMenu(isOpen => !isOpen);
-  }
-
-  const changeSearch = (e) => {
-    setSearch(e.target.value);
-  }
-  const clickSearch = () => {
-    console.log(searchWord);
-    // 백엔드에 searchWord전송
-  }
+    setMenu((isOpen) => !isOpen);
+  };
 
   const loginModal = () => {
     setLogin(true);
-  }
+  };
 
-  return(
+  return (
     <Main>
-
-      {
-        loginOpen && <Login setLogin={setLogin} />
-      }
+      {loginOpen && <Login setLogin={setLogin} />}
 
       <Wrap>
         <GotoHome>
-          <Link to='/'>
-            <span>
-              CODIARY
-            </span>
+          <Link to="/">
+            <span>CODIARY</span>
           </Link>
         </GotoHome>
 
         <SearchWrap>
-          <SearchBox className='search-box'>
-            <Search type='text' className='search' 
-              placeholder='SEARCH' onChange={changeSearch} value={searchWord}></Search>
-            <ion-icon size='small' name="search-outline"
-              onClick={clickSearch}></ion-icon>
-          </SearchBox>
+          <BtnSearch>
+            <Link to="/search">
+              <ion-icon
+                size="small"
+                name="search-outline"
+              ></ion-icon>
+            </Link>
+          </BtnSearch>
 
           <Profile>
-            <div className='userBox'>
-              <div className='imgBox' img='../IMG/profile_test.png'>
-              </div>
+            <div className="userBox">
+              <div className="imgBox" img="../IMG/profile_test.png"></div>
             </div>
-            <div className={isOpen ? 'menuToggleON' : 'menuToggleOFF'} onClick={toggleMenu}></div>
+            <div
+              className={isOpen ? "menuToggleON" : "menuToggleOFF"}
+              onClick={toggleMenu}
+            ></div>
           </Profile>
 
           <Menu>
-            <div className={isOpen ? 'menuON' : 'menuOFF'}>
-              <Link className='tagP' to='/:userId'>내 글 목록</Link>
-              <Link className='tagP' to='/:userId/calender'>내 코디어리</Link>
-              <Link className='tagP' to='/:userId/presave'>임시글 목록</Link>
-              <Link className='tagP' to='/:userId/visite'>방문&좋아요 목록</Link>
-              <Link className='tagP' to='/setting'>설정</Link>
-              <p className='logout tagP' onClick={loginModal}>로그인</p>
+            <div className={isOpen ? "menuON" : "menuOFF"}>
+              <Link className="tagP" to="/:userId">
+                내 글 목록
+              </Link>
+              <Link className="tagP" to="/:userId/calender">
+                내 코디어리
+              </Link>
+              <Link className="tagP" to="/:userId/presave">
+                임시글 목록
+              </Link>
+              <Link className="tagP" to="/:userId/visite">
+                방문&좋아요 목록
+              </Link>
+              <Link className="tagP" to="/setting">
+                설정
+              </Link>
+              <p className="logout tagP" onClick={loginModal}>
+                로그인
+              </p>
             </div>
-
           </Menu>
         </SearchWrap>
       </Wrap>
-
     </Main>
-
-  )
-}
+  );
+};
 export default SearchProfile;
