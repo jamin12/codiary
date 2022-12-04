7db2e2292932787d1f824c26965822c08b19164a
jamin12 <rudals9901@naver.com>
2022-12-05 00:19:56 +0900

    style: presave
    1. css 변경

--- client/src/Pages/PreSavePost.js
+++ client/src/Pages/PreSavePost.js
@@ -83,35 +83,6 @@ const Wrap = styled.div`
 		width: 90%;
 	}
 `;
-// 카테고리박스
-const CategoryBox = styled.div`
-	position: relative;
-	display: block;
-
-	width: 13%;
-	z-index: 100;
-	.category {
-		position: sticky;
-		top: 250px;
-		border-left: 2px solid rgba(173, 167, 167, 0.7);
-		box-sizing: border-box;
-		padding: 15px 0 15px 10px;
-	}
-	.category ul {
-		box-sizing: border-box;
-		margin-top: 5px;
-	}
-	.category ul li {
-		width: 80%;
-		text-overflow: ellipsis;
-		overflow: hidden;
-		white-space: nowrap;
-	}
-	@media screen and (max-width: 1024px) {
-		display: none;
-		margin: 0 auto;
-	}
-`;
 const ContentWrapBox = styled.div`
 	width: 70%;
 	margin: 0 auto;
@@ -124,15 +95,19 @@ const ContentWrapBox = styled.div`
 	}
 `;
 const HeaderBox = styled.div`
+	width: 100%;
 	display: flex;
 	position: relative;
 	border-bottom: 2px solid #c8c8c8;
-	.title {
-		width: 100%;
-		font-weight: bold;
-		font-size: 60px;
-		text-align: center;
-	}
+  .title {
+    width: 70%;
+    height: 100%;
+    word-break: keep-all;
+    margin: 0 auto;
+    font-weight: bold;
+    font-size: 2.5rem;
+    text-align: center;
+  }
 	span {
 		position: absolute;
 	}
@@ -174,175 +149,5 @@ const ContentBox = styled.div`
 		p {
 			font-size: 1rem;
 		}
-	}
-`;
-const TagVisiteBox = styled.div`
-	margin: 0 auto 5px auto;
-	display: flex;
-	justify-content: space-between;
-
-	padding-bottom: 8px;
-	border-bottom: 2px solid #c8c8c8;
-
-	@media screen and (max-width: 1600px) {
-		.tag-box {
-			font-size: 0.89rem;
-		}
-	}
-`;
-const TagBox = styled.div`
-	display: flex;
-	max-width: 600px;
-	p {
-		border: 1px solid #000;
-		border-radius: 50px;
-		padding: 3px 20px;
-		margin: 0 15px;
-		margin-right: 5px;
-	}
-`;
-const VisiteBox = styled.div`
-	display: flex;
-	width: 170px;
-	justify-content: space-between;
-	margin: 0 10px;
-	.total-visite,
-	.today-visite,
-	.like-good {
-		display: flex;
-		align-items: center;
-		font-size: 12px;
-		line-height: 30px;
-	}
-	.total-visite ion-icon,
-	.today-visite ion-icon,
-	.like-good ion-icon {
-		width: 20px;
-		height: 20px;
-		display: inline-block;
-		margin: 0 3px;
-	}
-	.like-good {
-		position: relative;
-		width: 40px;
-		height: 30px;
-		display: flex;
-	}
-	.like-good p {
-		position: absolute;
-		right: 2px;
-	}
-	.like-good ion-icon {
-		position: absolute;
-		top: 50%;
-		left: 0;
-		transform: translateY(-50%);
-	}
-	.like-good > input {
-		display: none;
-	}
-`;
-const CommentBox = styled.div`
-	margin-top: 30px;
-	/* 댓글 창 */
-	.text-box {
-		width: 90%;
-		margin: 0 auto 10px;
-	}
-
-	@media screen and (max-width: 1600px) {
-		width: 97%;
-		margin-left: auto;
-		margin-right: auto;
-	}
-	@media screen and (max-width: 1024px) {
-		width: 90%;
-	}
-`;
-const ReplyBox = styled.div`
-	/* width: 60%; */
-	margin-top: 15px;
-	margin-left: 40px;
-	.text-box {
-		width: 90%;
-		margin: 0 auto 10px;
-	}
-
-	@media screen and (max-width: 1600px) {
-		width: 93%;
-	}
-	@media screen and (max-width: 1024px) {
-		width: 85%;
-		margin-left: 120px;
-		background-color: red;
-	}
-`;
-/* 날짜박스 */
-const DateBox = styled.div`
-	display: flex;
-	width: 90%;
-	margin: 0 auto;
-	align-items: flex-end;
-	.btn-reply {
-		cursor: pointer;
-		font-size: 0.9rem;
-		margin-right: 10px;
-	}
-	.date {
-		font-size: 0.8rem;
-		color: #9e9e9e;
-	}
-`;
-const ProfileBox = styled.div`
-	display: flex;
-	padding-bottom: 5px;
-	p {
-		font-size: 1.2rem;
-		text-align: center;
-		margin-top: -2px;
-	}
-	/* 프로필 이미지 */
-	img {
-		width: 30px;
-		height: 30px;
-		background-color: palevioletred;
-		border-radius: 50%;
-		overflow: hidden;
-		margin-right: 10px;
-	}
-	@media screen and (max-width: 1600px) {
-		img {
-			width: 25px;
-			height: 25px;
-		}
-	}
-`;
-// 서브타이틀 박스
-const SubTitleBox = styled.div`
-	width: 13%;
-	z-index: 100;
-	right: 0;
-	ul {
-		position: sticky;
-		top: 250px;
-		border-left: 2px solid rgba(173, 167, 167, 0.7);
-		padding: 15px 0;
-	}
-	ul li {
-		width: 80%;
-		margin: 0 auto;
-		text-overflow: ellipsis;
-		overflow: hidden;
-		white-space: nowrap;
-	}
-	ul li::before {
-		content: '" ';
-	}
-	ul > li::after {
-		content: ' "';
-	}
-	@media screen and (max-width: 1024px) {
-		display: none;
-		margin: 0 auto;
 	}
 `;
