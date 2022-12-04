1507fc93b94fc7c004955c5c15bae4cd4f7ce549
leemyou <limhyeon0809@gmail.com>
2022-12-04 21:50:01 +0900

    Feat: 신고 리스트 limit값

--- client/src/components/SettingReport.jsx
+++ client/src/components/SettingReport.jsx
@@ -61,6 +61,7 @@ const SettingReport = () => {
           startDate: Sdate,
           endDate: Edate,
           offset: 0,
+          limit: 20
         },
         withCredentials: true
       })
